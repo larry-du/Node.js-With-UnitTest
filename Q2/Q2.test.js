5633583957248675f4bbcaf5b7ab6387194dd174
Larry <shaka422@hotmail.com>
2020-05-08 21:00:56 +0800

    Q2test更改
    對格式判斷新增一組測試 符合程式邏輯

--- Q2/Q2.test.js
+++ Q2/Q2.test.js
@@ -1,8 +1,16 @@
 const main = require("./Q2");
 
 test('emptyStringToBeFalse 空字串!! 請重新輸入', () => {
-    execute = main.setInput(" ")
+    const strInput1 = " ";
+    execute = main.setInput(strInput1);
     expect(execute).toEqual({
+        status: false,
+        errorMessage: " !!請勿輸入空字串!! 請重新輸入",
+        result: null
+    });
+    const strInput2 = " ";
+    execute2 = main.setInput(strInput2);
+    expect(execute2).toEqual({
         status: false,
         errorMessage: " !!請勿輸入空字串!! 請重新輸入",
         result: null
@@ -10,17 +18,33 @@ test('emptyStringToBeFalse 空字串!! 請重新輸入', () => {
 });
 
 test('floatToBeFalse 小數!! 請重新輸入', () => {
-    const execute = main.setInput("12.12");
+    const strInput1 = "12.12";
+    const execute = main.setInput(strInput1);
     expect(execute).toEqual({
         status: false,
         errorMessage: "12.12!!請勿輸入小數!! 請重新輸入",
         result: null
     });
+    const strInput2 = "12.34";
+    const execute2 = main.setInput(strInput2);
+    expect(execute2).toEqual({
+        status: false,
+        errorMessage: "12.34!!請勿輸入小數!! 請重新輸入",
+        result: null
+    });
 });
 
 test('StringToBeFalse 字串!! 請重新輸入', () => {
-    const execute = main.setInput("stringTest");
+    const strInput1 = "stringTest";
+    const execute = main.setInput(strInput1);
     expect(execute).toEqual({
+        status: false,
+        errorMessage: "stringTest!!請勿輸入字串!! 請重新輸入",
+        result: null
+    });
+    const strInput2 = "stringTest";
+    const execute2 = main.setInput(strInput2);
+    expect(execute2).toEqual({
         status: false,
         errorMessage: "stringTest!!請勿輸入字串!! 請重新輸入",
         result: null
@@ -28,15 +52,19 @@ test('StringToBeFalse 字串!! 請重新輸入', () => {
 });
 
 test('sameReminder 於數相同', () => {
-    main.setInput("12");
-    main.setInput("12");
+    const strInput1 = "12";
+    const strInput2 = "12";
+    main.setInput(strInput1);
+    main.setInput(strInput2);
     const execute = main.resultOfDiv3();
     expect(execute).toMatch("餘數相同");
 });
 
 test('notSameReminder 於數不相同', () => {
-    main.setInput("12");
-    main.setInput("13");
+    const strInput1 = "12";
+    const strInput2 = "13";
+    main.setInput(strInput1);
+    main.setInput(strInput2);
     const execute = main.resultOfDiv3();
     expect(execute).toMatch("餘數不相同");
 });
--- Q2/userInterfaceInput.js
+++ Q2/userInterfaceInput.js
@@ -1,11 +1,11 @@
 //### 2. 使用者輸入兩個整數，將它們分別除以 3 ，判斷餘數是否相同，若相同，則於螢幕上顯示「餘數相同」。
-const main2 = require("./Q2");
+const main = require("./Q2");
 const rl = require("./readline");
 
 (function askRemainderQuestion(rl) {
     rl.question('請輸入第一組整數:', strInput1 => {
         // const firstConfirmFormat = confirmFormat(strInput1);
-        const calculation = main2.setInput(strInput1);
+        const calculation = main.setInput(strInput1);
         // 判斷格式是否正確（小數 字串 NaN 空字串）不正確再重新詢問
         // if (!!firstConfirmFormat) {
         //     console.log(firstConfirmFormat);
@@ -23,8 +23,7 @@ const rl = require("./readline");
 function askQuestion2(strInput1) {
     rl.question('請輸入第二組整數:', strInput2 => {
         // 判斷格式是否正確（小數 字串 NaN 空字串）不正確再重新詢問
-        const calculation = main2.setInput(strInput2);
-
+        const calculation = main.setInput(strInput2);
         // const calculation = main(strInput1, strInput2);
         if (!calculation.status) {
             console.log(calculation.errorMessage);
@@ -39,7 +38,7 @@ function askQuestion2(strInput1) {
         //如果不相同
         //顯示餘數不相同
         //程式結束
-        console.log(main2.resultOfDiv3());
+        console.log(main.resultOfDiv3());
         rl.close();
     });
 }
