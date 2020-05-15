370c696956ec394ae6129c0383bcd720497e17dc
Larry <shaka422@hotmail.com>
2020-05-15 12:58:49 +0800

    Q3檔案名稱修改

--- Q3/discountInterface.js
+++ Q3/discountInterface.js
@@ -1,5 +1,5 @@
 //### 3. 好想樂園的門票一張400元，優惠6歲(含)以下的孩童和65歲(含)以上的老人半價，試用「邏輯運算子」寫一程式，讓使用者自行輸入年齡，並判斷門票價錢。
-const main = require("./Q3")
+const main = require("./Q3-module")
 const rl = require("./readline");
 (function discount(rl) {
     rl.question('請輸入年齡:', strInput => {
